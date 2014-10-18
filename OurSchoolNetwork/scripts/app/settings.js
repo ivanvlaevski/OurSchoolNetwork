89353274a6602d175f7740083b529b373299e455
Ivan Vlaevski <ivlaevski@objectsystems.com>
2014-10-18 07:47:58 +0000

    Changes made by Ivan Vlaevski (ivlaevski@objectsystems.com).

--- OurSchoolNetwork/scripts/app/settings.js
+++ OurSchoolNetwork/scripts/app/settings.js
@@ -5,7 +5,7 @@
 var appSettings = {
 
     everlive: {
-        apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
+        apiKey: 'BH5VM9dAdA5YhDtc', // Put your Backend Services API key here
         scheme: 'http'
     },
 
