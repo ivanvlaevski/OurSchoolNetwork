0b7ef82d6a23a10b768bdb853359b29ee49349c3
d <d>
2014-10-18 13:02:06 +0300

    geolocation collected

--- OurSchoolNetwork/scripts/app/GeoLocationService.js
+++ OurSchoolNetwork/scripts/app/GeoLocationService.js
@@ -1,5 +1,5 @@
 /**
- * Login view model
+ * GeoLocationService
  */
 
 var app = app || {};
@@ -7,35 +7,30 @@ var app = app || {};
 app.GeoLocationService = (function () {
     'use strict';
 
-    var startWatch = function () {
-        if (appSettings.watchId != null) {
-            navigator.geolocation.clearWatch(appSettings.watchId);
-        };
-        var options = { options: 10000, enableHighAccuracy: true };
-        appSettings.watchId = navigator.geolocation.watchPosition(function () {
-            app.GeoLocationService.onSuccess(app.GeoLocationService, arguments);
-        }, function () {
-            app.GeoLocationService.onError(app.GeoLocationService, arguments);
-        }, options);
-    };
-
-    var onSuccess = function (position) {
-        appSettings.currentLocation = new Everlive.GeoPoint(position.coords.latitude,positon.coords.longitude);
-    };
-
-    var onError = function (error) {
-        alert(error);
-    };
 
 
     return {
-        startWatch: startWatch,
-        onSuccess: onSuccess,
-        onError: onError
+        startWatch: function () {
+            if (appSettings.watchId != null) {
+                navigator.geolocation.clearWatch(appSettings.watchId);
+            };
+            var options = { options: 10000, enableHighAccuracy: true };
+            appSettings.watchId = navigator.geolocation.watchPosition(function () {
+                app.GeoLocationService.onSuccess(this,arguments);
+            }, function () {
+                app.GeoLocationService.onError(this,arguments);
+            }, options);
+        },
+            
+        onSuccess : function (a,position) {
+            appSettings.currentLocation = { pos: position, error: null };// new Everlive.GeoPoint(position.coords.latitude,positon.coords.longitude);
+        },
+        
+        onError: function (a,error) {
+            // alert(error);
+            appSettings.currentLocation = { pos: null, error: error };
+        }
     };
-
-
-
 
 
 }());
